2288fca38d85e4a0de3094acce00ddfc52b7f5a1
alexanderxablaza <alexanderxablaza@gmail.com>
2024-05-13 09:29:58 +0800

    test user mgt

---
+++ UnitTest/UserManagement-SampleApp/src/UserManagement.Tests.cs
@@ -0,0 +1,34 @@
+﻿namespace UnitTest.UserManagement_SampleApp.src;
+public class UserManagementTests
+{
+    private readonly UserManagement _svc;
+    public UserManagementTests()
+    {
+        _svc = new UserManagement();
+    }
+    [Fact]
+    public void Method_Add_Insert_User()
+    {
+        //arrange
+        const string s = "something";
+        User user = new("Alex", "xelA")
+        {
+            Id = 1,
+            Email = s,
+            CreatedDate = DateTime.Now
+        };
+        //act
+        _svc.Add(user);
+        //assert
+        var savedUser = Assert.Single(_svc.Users);
+        Assert.True(_svc.Users.Any());
+        Assert.Equal("Alex", savedUser.FirstName);
+        Assert.Equal("xelA", savedUser.LastName);
+        var props = typeof(User).GetProperties();
+        foreach (var p in props)
+        {
+//            Console.WriteLine("{prop name} : {prop value}", f[i].GetValue(savedUser));
+            Assert.Equal(p.GetValue(user, null), p.GetValue(savedUser, null));
+        }
+    }
+}
---
+++ UnitTest/UserManagement-SampleApp/src/UserManagement.cs
@@ -0,0 +1,24 @@
+﻿namespace UnitTest.UserManagement_SampleApp.src;
+internal class UserManagement
+{
+    private readonly List<User> _users = [];
+    public IEnumerable<User> Users => _users;
+    private int @count = 1;
+    public void Add(User user) => _users.Add(user with { Id = @count++});
+    public void UpdatePhone(User user) => _users.First(u => u.Id == user.Id).Phone = user.Phone;
+    public void Verify(User user) => _users.First(u => u.Id == user.Id).Verified = true;
+
+    public static void Main()
+    {
+        UserManagement t = new();
+        t.Add(user: new("Alex", "Xela") { Id = 0, Email = "", CreatedDate = DateTime.MinValue });
+    }
+}
+public record User(string FirstName, string LastName)
+{
+    public required int Id { get; set; }
+    public required DateTime CreatedDate { get; set; } = DateTime.Now;
+    public string? Phone { get; set; }
+    public required string Email { get; set; } = FirstName.ToLower().Trim() + "." + LastName.ToLower().Trim() + "@email.com";
+    public bool Verified { get; set; } = false;
+}
